ee6679e2cea1c4d282c6451d001d4d850b5c33e5
yaroslav <yaroslavzhyman84@gmail.com>
2026-09-17 20:00:31 +0300

    Render first OpenGL triangle

--- src/Engine.Native/Program.cs
+++ src/Engine.Native/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.IO;
 using Engine.Core;
 using Silk.NET.Windowing;
 using Silk.NET.Maths;
@@ -10,6 +11,8 @@ static class Program
 {
     private static GL _gl = null!;
     private static IWindow _window = null!;
+    private static uint _vao;
+    private static uint _shaderProgram;
     
     static void Main()
     {
@@ -38,14 +41,70 @@ static class Program
         engineRuntime.Tick(deltaTime);
     }
 
-    static void OnLoad()
+    static unsafe void OnLoad()
     {
         _gl = _window.CreateOpenGL();
+        _gl.ClearColor(Color.CornflowerBlue);
+
+        float[] vertices =
+        {
+            0.0f, 0.5f,
+            -0.5f, -0.5f,
+            0.5f, -0.5f
+        };
+
+        uint vbo = _gl.GenBuffer();
+        _gl.BindBuffer(BufferTargetARB.ArrayBuffer, vbo);
+        _gl.BufferData(BufferTargetARB.ArrayBuffer, (uint)(sizeof(float) * vertices.Length), vertices, BufferUsageARB.StaticDraw);
+
+        _vao = _gl.GenVertexArray();
+        _gl.BindVertexArray(_vao);
+        _gl.VertexAttribPointer(0u, 2, VertexAttribPointerType.Float, false, (uint)(2 * sizeof(float)), (void*)(0));
+        _gl.EnableVertexAttribArray(0);
+
+        string vertexSource = File.ReadAllText(@"/mnt/hdd2/ugc-platform/src/Engine.Native/Shaders/basic.vert");
+        string fragmentSource = File.ReadAllText(@"/mnt/hdd2/ugc-platform/src/Engine.Native/Shaders/basic.frag");
+
+        uint vertexShader = _gl.CreateShader(ShaderType.VertexShader);
+        uint fragmentShader = _gl.CreateShader(ShaderType.FragmentShader);
+        
+        _gl.ShaderSource(vertexShader, vertexSource);
+        _gl.ShaderSource(fragmentShader, fragmentSource);
+        
+        _gl.CompileShader(vertexShader);
+        _gl.GetShader(vertexShader, ShaderParameterName.CompileStatus, out int vertexStatus);        
+        _gl.CompileShader(fragmentShader);
+        _gl.GetShader(fragmentShader, ShaderParameterName.CompileStatus, out int fragmentStatus);
+
+        if (vertexStatus == 0)
+        {
+            string log = _gl.GetShaderInfoLog(vertexShader);
+            Console.WriteLine(log);
+        }
+        if (fragmentStatus == 0)
+        {
+            string log = _gl.GetShaderInfoLog(fragmentShader);
+            Console.WriteLine(log);
+        }
+
+        _shaderProgram = _gl.CreateProgram();
+        _gl.AttachShader(_shaderProgram, vertexShader);
+        _gl.AttachShader(_shaderProgram, fragmentShader);
+        _gl.LinkProgram(_shaderProgram);
+        _gl.GetProgram(_shaderProgram, GLEnum.LinkStatus, out int programStatus);
+
+        if (programStatus == 0)
+        {
+            string log = _gl.GetProgramInfoLog(_shaderProgram);
+            Console.WriteLine(log);
+        }
     }
 
     static void OnRender(double deltaTime)
     {
-        _gl.ClearColor(Color.CornflowerBlue);
         _gl.Clear(ClearBufferMask.ColorBufferBit);
+        _gl.UseProgram(_shaderProgram);
+        _gl.BindVertexArray(_vao);
+        _gl.DrawArrays(PrimitiveType.Triangles, 0, 3);
     }
 }
--- src/Engine.Sandbox/obj/Debug/net10.0/Engine.Sandbox.AssemblyInfo.cs
+++ src/Engine.Sandbox/obj/Debug/net10.0/Engine.Sandbox.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Engine.Sandbox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb2420c0e5b33c06ebea903460faba0e6bec1b09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2716d3a625aa10e84437339325123c308d9fd05a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Engine.Sandbox")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Engine.Sandbox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
